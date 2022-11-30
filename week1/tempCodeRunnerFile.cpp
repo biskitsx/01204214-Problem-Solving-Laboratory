347ac2cb758c966a25a5ef1e9ac8fe1c5ab5c705
biskitsx <112327677+biskitsx@users.noreply.github.com>
2022-11-30 10:48:37 +0700

    Delete tempCodeRunnerFile.cpp

--- week1/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-    // for (i=0;i<n-1;i++) {
-    //     for (j=i+1;j<n;j++) {
-    //         if (vect[i] < vect[j]) {
-    //             intersection++ ;
-    //         }
-    //     }
-    // }
