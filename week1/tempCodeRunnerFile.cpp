dacf0d513dae2618f098f0ad6fd03a39729f33f6
biskitsx <112327677+biskitsx@users.noreply.github.com>
2022-11-30 10:48:00 +0700

    PPPPPPPPPP

---
+++ week1/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+    // for (i=0;i<n-1;i++) {
+    //     for (j=i+1;j<n;j++) {
+    //         if (vect[i] < vect[j]) {
+    //             intersection++ ;
+    //         }
+    //     }
+    // }
