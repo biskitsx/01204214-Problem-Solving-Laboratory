a5c10c4d6bfff71bebca88102811e6047e4ed05a
biskitsx <112327677+biskitsx@users.noreply.github.com>
2022-11-30 09:23:23 +0700

    PPPPPP--

--- week1/running.cpp
+++ week1/running.cpp
@@ -6,8 +6,6 @@ using namespace std ;
 int main() {
     int n,k,i,tpr,fastest ;
     unordered_map <int,int> tprs ; 
-    unordered_map <int,int> map ;
-
 
     //input 
     cin >> n >> k ; 
@@ -15,7 +13,7 @@ int main() {
         cin >> tpr ; 
         tprs[i] = tpr ; 
     }
-    map = tprs ;
+
     //find fastest 
     fastest = tprs[0] ; 
     for (i=0;i<n;i++) {
@@ -26,30 +24,13 @@ int main() {
     }
 
     int runnerLeft = n ;
-    int currentTime = fastest ;
-    int round = 2 ;
-    while (1) {
-        if (round == k+1) {
-            break ; 
+    fastest *= k ;
+    for (i = 0; i< n;i++) {
+        if (tprs[i]*(k-1) >= fastest) {
+            runnerLeft--;
         }
-        currentTime += fastest ;
-        for (i=0;i<n;i++) {
-            if (tprs[i]==0) {
-                continue;
-            }
-            if (currentTime <= tprs[i]) {
-                runnerLeft-- ; 
-                tprs[i] = 0; 
-                continue;
-            }
-            tprs[i] += map[i] ; 
-        }
-        round++ ; 
     }
 
     cout << runnerLeft ;
-
-
-    
 
 }
