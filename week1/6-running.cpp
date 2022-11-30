2db9874472988d287f788c512c9a12b8bd787ca7
biskitsx <112327677+biskitsx@users.noreply.github.com>
2022-11-30 14:47:00 +0700

    [PPPPPP--]

--- week1/6-running.cpp
+++ week1/6-running.cpp
@@ -1,32 +1,29 @@
 #include<iostream>
 #include<vector>
-#include<unordered_map>
 using namespace std ;
 
 int main() {
-    int n,k,i,tpr ;
-    int fastest = 100000 ;
-    unordered_map <int,int> tprs ; 
+    int n,k,i,time;
+    float fastest = 100000 ;
+    vector <float> timesPerRound ;
 
     //input and find fastest
-    cin >> n >> k ; 
-    for( i = 0 ; i < n; i++ ) {
-        cin >> tpr ; 
-        tprs[i]= tpr ; 
-        if (tpr < fastest) {
-            fastest = tpr ; 
-        }
+    cin >> n >> k ;
+    for (i=0;i<n;i++) {
+        cin >> time ; 
+        timesPerRound.push_back(time) ; 
+        fastest = time < fastest ? time : fastest ; 
     }
 
-
-    //comparison
-    int runnerLeft = n ;
-    fastest *= k ;
-    for (i = 0; i< n;i++) {
-        if (tprs[i]*(k-1) >= fastest) {
-            runnerLeft--;
+    //find runner left 
+    int runnerLeft = n  ;
+    //max Time for running
+    float maxTime = fastest * k ;
+    for (i=0;i<n;i++) {
+        float roundRunner = maxTime/timesPerRound[i] ; 
+        if (roundRunner <= k-1) {
+            runnerLeft-- ; 
         }
     }
-    cout << runnerLeft ;
-
+    cout << runnerLeft ; 
 }
