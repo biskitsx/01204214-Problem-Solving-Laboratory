e5f33c0480f89ab3471b67eea8b2c02a91de9dc5
biskitsx <112327677+biskitsx@users.noreply.github.com>
2022-12-21 09:59:52 +0700

    [-----------]

--- week2/8-cars.cpp
+++ week2/8-cars.cpp
@@ -1,3 +1,23 @@
 #include<iostream>
+#include<vector>
 using namespace std ;
-int main( )
+
+int main( )  {
+    int n , i , quality , price   ;
+    vector <int> carQuality ; 
+    int cantSold = 0 ;
+    cin >> n ;
+
+    for (i=0;i<n;i++){
+        cin >> price >> quality ; 
+        carQuality.push_back(quality) ; 
+    }
+
+    for (i=0;i<n-1;i++) {
+        if (carQuality[i] <= carQuality[i+1]) {
+            cantSold++ ;
+        }
+    }
+
+    cout << cantSold <<endl ; 
+}
