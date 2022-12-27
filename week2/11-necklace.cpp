4b2566544b20e13a905ea6dfaa160d84822f4122
biskitsx <112327677+biskitsx@users.noreply.github.com>
2022-12-27 09:05:05 +0700

    [---TTTTTTT]

--- week2/11-necklace.cpp
+++ week2/11-necklace.cpp
@@ -2,12 +2,52 @@
 using namespace std; 
 
 int main() {
-    int n, i, next, begin ;
-    int neckLace[3000001] ; 
-    
-    
+    int n, i, next, begin ,connect;
+    int neckLace[300001] = {0}; 
+
+    // cout << neckLace[4] ;
+        
     cin >>  n ; 
+    int sum = n*(n+1)/2 ; 
+
     for (i=0;i<n-1;i++) {
         cin >> next >> begin ;
+        // sum -= begin ; 
+        // cout << sum << endl ;
+
+        //begin is never use 
+        // cout << neckLace[begin] ; 
+        if (neckLace[begin] == 0) {
+            neckLace[begin] = next ;
+            sum -= begin ; 
+            // cout << sum << endl ; 
+        }
+
+        //begin is already used 
+        else {
+            connect = neckLace[begin] ;
+            neckLace[begin] = next ;
+            int lastBegin = begin ;
+            while(1) {
+                if (neckLace[lastBegin]==0) {
+                    break;
+                }
+                lastBegin = neckLace[lastBegin] ;
+            }
+            neckLace[lastBegin] = connect ;
+            sum -= lastBegin ; 
+            // cout << sum << endl ; 
+        }
+    }
+
+    int start = n - sum ;
+    cout << start << ' '; 
+    while(1) {
+        if (neckLace[neckLace[start]] == 0) {
+            cout << neckLace[start] ;
+            break;
+        }
+        cout << neckLace[start] << ' ';
+        start = neckLace[start] ; 
     }
 }
