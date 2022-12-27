0872900f9b6502e2733ec5d0f09a6960a10adee3
biskitsx <112327677+biskitsx@users.noreply.github.com>
2022-12-27 14:53:34 +0700

    [ PPPPPPPPPPPP ]

--- week3/robot1000s.cpp
+++ week3/robot1000s.cpp
@@ -3,28 +3,71 @@ using namespace std ;
 
 int main() {
     string text ;
-    int x=0, y=0, i, k  ; 
+    int i, k  ; 
+    int N = 0 , E = 0 , S = 0 , W = 0 ; 
+    int lenghtY, lenghtX ;
     cin >> text ;
     cin >> k ; 
     for (i=0;i<text.size();i++) {
+
         if ('E' == text[i]) {
-            x++ ;
+            E++;
         }
         else if ('W' == text[i]) {
-            x-- ;
+            W++;
         }
-
 
         else if ('N' == text[i]) {
-            y++ ; 
+            N++ ;
         }
         else if ('S' == text[i]) {
-            y-- ; 
+            S++ ;
+        }
+    }
+
+    //consider y
+    if (S==0||N==0) {
+        lenghtY = N > S ? N : S ;
+    }
+
+    else {
+        int diffSN = abs(S-N) ; 
+        int maxSN = N > S ? N : S ;
+        int minSN = N < S ? N : S ;
+
+        if (minSN > k) {
+            minSN -= k ;
+            k = 0 ;
+        }
+        else {
+            k -= minSN ;
+            minSN =0 ;
         }
 
-
+        lenghtY = maxSN - minSN ; 
     }
-    cout << (abs(x) + abs(y) + k)*2 ;
-    
-    
+
+
+    if (E==0||W==0) {
+        lenghtX = E > W ? E : W ;
+    }
+
+    else {
+        int diffEW = abs(E-W) ; 
+        int maxEW = E > W ? E : W ;
+        int minEW = E < W ? E : W ;
+
+        if (minEW > k) {
+            minEW -= k ;
+            k = 0 ;
+        }
+        else {
+            k -= minEW ;
+            minEW =0 ;
+        }
+        lenghtX = maxEW - minEW ;
+    }
+
+    cout << (lenghtX+lenghtY-k)*2 ; 
+
 }
