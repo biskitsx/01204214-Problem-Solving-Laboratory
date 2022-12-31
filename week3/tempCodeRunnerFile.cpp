c4afae085e75356cfaed36a67fbbf4d1d518a6f5
biskitsx <112327677+biskitsx@users.noreply.github.com>
2022-12-31 21:19:44 +0700

    ...

--- week3/tempCodeRunnerFile.cpp
+++ week3/tempCodeRunnerFile.cpp
@@ -1,33 +1,3 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-using namespace std;
-
-int main() {
-    int n ,i ,j ,max;
-    int codingLevel ; 
-    int winnerIndex;  
-    vector <int> winner ; 
-    int challengerIndex[10000] ; 
-    
-    cin >> n ;
-
-    for (i=0;i<4;i++) {
-        max = 0 ;
-        for (j=0;j<n;j++) {
-            cin >> codingLevel ; 
-            if (max < codingLevel) {
-                max = codingLevel ; 
-                winnerIndex = i*n + j + 1 ;
-            }
-        }
-        auto it = upper_bound(winner.cbegin(), winner.cend(), max, greater<int>());
-        winner.insert(it, max);
-        challengerIndex[max] = winnerIndex ; 
-    }
-
-    
-    for (auto e: winner) {
-        cout << challengerIndex[e] << ' '; 
-    }
-}
+    // for(int u=1; u<=n; u++) {
+    //     cout << u << " " << layer[u] << endl;
+    // }
