a1b3e45161e716cec510e1858d6b2de332a0db15
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-01-01 22:13:49 +0700

    [ PPPPPPPP-P ]

--- week3/jumppoint.cpp
+++ week3/jumppoint.cpp
@@ -1,67 +1,83 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <queue> 
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<queue>
 
-using namespace std;
+using namespace std ;
 
 int n,r,i,j ;
-float u,v ; 
+int u,v  ;
 
-vector <pair <float,int> > map[101] ;
-pair <float,float> coor[101] ; 
+vector <int> adjList[100001] ;
+pair <int,int> coor[100001] ;
+int layer[100001] = {-1} ;
 
 
-pair <float,float> createPair(float u,float v) {
-    pair <float,float> p ; 
+void createPair(int index,int u,int v) {
+    pair <int,int> p ;
     p.first = u ;
     p.second = v ;
-    return p ;
+    coor[index] = p ;
 }
 
-pair <float,int>  createNode(float weight, int index) {
-    pair <float,int> p ;
-    p.first = weight ;
-    p.second = index; 
-    return p ; 
-}
+void checkR(int oldCoor) {
+    float oldX = coor[oldCoor].first*1.0 ;
+    float oldY = coor[oldCoor].second*1.0 ;
+    float distance = sqrt((oldX-u*1.0)*(oldX-u*1.0)+(oldY-v*1.0)*(oldY-v*1.0)) ;
 
+    if (distance <= r) {
+        adjList[oldCoor].push_back(i) ; 
+        adjList[i].push_back(oldCoor) ; 
+    }
+}
 void readInput() {
     cin >> n >> r ;
 
-    coor[0] = createPair(0,0) ;
-    coor[n+1] = createPair(100,100) ;
+    //init
+    createPair(0,0,0) ;
+    createPair(n+1,100,100) ;
 
     for (i=1;i<=n;i++) {
         cin >> u >> v ;
         
-        coor[i] = createPair(u,v) ;
+        createPair(i,u,v) ;
 
         for (j=0;j<i;j++) {
-            float oldX = coor[j].first ;
-            float oldY = coor[j].second ;
-            
-            float distance = sqrt((oldX-u)*(oldX-u)+(oldY-v)*(oldY-v)) ;
-
-            //connect 
-            if (distance <= r) {
-                map[i].push_back(createNode(distance,j)) ; 
-                map[j].push_back(createNode(distance,i)) ; 
-            }
+            checkR(j) ; 
         }
-
-        float distance = sqrt((100-u*1.0)*(100-u)+(100-v)*(100-v)) ;
-        
-        if (distance <= r) {
-            map[i].push_back(createNode(distance,n+1)) ; ; 
-            map[n+1].push_back(createNode(distance,i)) ; 
-        }
-        
+        checkR(n+1) ; 
     }
+
 }
 
+void bfs() {
+    queue <int> q ;
+    int start = 0 ;
+    bool visited[100000] ={false}; 
+    layer[start]=  0;
+    
+    visited[start] = true ;
+    q.push(start) ; 
 
+    while(!q.empty()) {
+        int curr = q.front() ;
+        // cout << curr << endl ;
+        q.pop() ; 
+
+        size_t i ;
+        for (i=0;i < adjList[curr].size();i++) {
+            int next = adjList[curr][i] ;
+            if (!visited[next]) {
+                visited[next] = true ;
+                layer[next] = layer[curr]+1 ;
+                q.push(next) ; 
+            }
+        }
+    }
+}
 int main() {
-    readInput() ;
+    readInput() ; 
+    bfs();
+    cout << (layer[n+1] > 0 ? layer[n+1] : -1); 
 
 }
