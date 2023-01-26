a445cf5e92514d9ff7e2d54fdbf4344ae8652ba9
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-01-26 13:30:23 +0700

    [ PPPPPPPPPPPP ]

--- week3/box.cpp
+++ week3/box.cpp
@@ -1,23 +1,67 @@
-#include <iostream>
-#include <vector>
-#include <queue> 
+#include<iostream>
+#include<vector>
 using namespace std;
 
-int n,m,i,j,x;
-vector <int> adj[31] ;
+char map[31][31] ;
 bool visited[31][31] ;
+bool go = false ;
+int n, m ;
+int i, j;
 
 void readInput() {
     cin >> n >> m ;
-    for (i=0;i<n;i++) {
-        for (j=0;j<m;j++) {
-            cin >> x ;
-            adj[i].push_back(x) ;
+    for (i=0 ; i < n ; ++i) {
+        for (j=0; j < m;j++) {
+            cin >> map[i][j] ;
+            if (map[i][j] == '#') {
+                visited[i][j] = true ;
+            }
+        }
+    } 
+}
+
+void travel(int x,int y) {
+
+    if (x == n-2 && map[x+1][y] == '.' && map[x][y+1] == '.' && map[x+1][y+1] =='.' ) {
+        go = true ; 
+        return ;
+    }
+
+    if (x < 0 || x >= m || y < 0 || y >= n) {
+        return ;
+    }
+
+    if (visited[x][y]) {
+        return ;
+    }
+
+    visited[x][y] = true; 
+
+    if (map[x+1][y] == '.' && map[x][y+1] == '.' && map[x+1][y+1] == '.') {
+        travel(x+1,y) ;
+        travel(x-1,y) ; 
+        travel(x,y+1) ; 
+        travel(x,y-1) ; 
+    }
+
+}
+
+void start() {
+    for (i=0;i<m-1;i++) {
+        if (map[0][i] == '.') {
+            travel(0,i) ; 
         }
     }
 }
 
-
 int main() {
+    readInput();
+    start() ;
 
+    if (go) {
+        cout << "yes\n";
+    }
+    else {
+        cout << "no\n" ; 
+    }
 }
