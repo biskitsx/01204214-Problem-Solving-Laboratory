d8c507e5e15753977eb6f33854cb49f3ae067b1b
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-02-14 15:54:23 +0700

    [ PPTPPTTTTTTTTTPTTTTT ]

--- week5/9-spread.cpp
+++ week5/9-spread.cpp
@@ -8,11 +8,30 @@ int n, m;
 int speed[100001] ;
 int u, v, w;
 int i, j;
-int second = 0 ;
-int countConnect ;
-vector<pair<int,int> > graph[100001] ;
-vector<int> connectedGraph[100001] ; 
-int connectCount ;
+
+double maxTime = 0xfffffff;
+int round = 0 ;
+int nub = 1 ;
+
+vector<pair<int,double> > graph[100001] ;
+
+double dist[100001] ;
+bool visited[100001] ;
+
+//input
+
+void reset() {
+    for (i = 1; i <= n; i++) {
+        dist[i] = 0xfffffff;
+        visited[i] = false ;
+    }
+}
+
+
+double findTime() {
+    double t = (speed[v]+speed[u])/w ; 
+    return t ;
+}
 
 void readInput() {
     cin >> n >> m ;
@@ -23,78 +42,58 @@ void readInput() {
 
     for (i=0;i<m;i++) {
         cin >> u >> v >> w;
-        graph[u].push_back(make_pair(v,w));
-        graph[v].push_back(make_pair(u,w));
+        double time = findTime() ; 
+
+        graph[u].push_back(make_pair(v,time));
+        graph[v].push_back(make_pair(u,time));
     }
 }
 
 void bfs() {
-    queue<int> q ;
-
-    vector<bool> visited ;
-    visited.resize(n+1,false) ;
-
+    reset() ; 
+    queue<int> q ; 
     q.push(1) ; 
-    visited[1] = true ; 
-    connectCount = 1 ;
-    
+    visited[1] = true ;
+
+    round++  ;
+    nub = 1 ;
+
     while(!q.empty()) {
-        int curr = q.front() ;
+        int curr = q.front() ; 
         q.pop() ; 
 
-        for (i = 0; i < connectedGraph[curr].size(); i++) {
-            int next = connectedGraph[curr][i] ; 
+        int i ;
+        for (i=0;i<graph[curr].size();i++) {
+            int next = graph[curr][i].first ;
+            double time = graph[curr][i].second ; 
+            
+            if (time > round*1.0) {
+                continue;
+            }
+
             if (!visited[next]) {
+                nub++ ; 
                 visited[next] = true ;
                 q.push(next) ; 
-                connectCount++ ; 
             }
+        
         }
+
     }
 }
 
-void updateDistance(int x,int y) {
-    graph[x][y].second -= speed[x] ; 
-    int next = graph[x][y].first, i ;  
-    for (i=0;i<graph[next].size();i++) {
-        if (graph[next][i].first == x) {
-            graph[next][i].second -= speed[x] ; 
-            // cout <<"after delete : "<< graph[next][i].second <<endl ;
-        }
-    }
-
-    if (graph[x][y].second <= 0) {
-        connectedGraph[x].push_back(graph[x][y].first) ; 
-        connectedGraph[graph[x][y].first].push_back(x) ; 
-    }
-
-}
-
-void eachSecond() {
-    for (i=1;i<=n;i++) {
-        for (j=0;j<graph[i].size();j++) {
-            if (graph[i][j].second <= 0) {
-                continue; 
-            }
-
-            updateDistance(i,j) ; 
-            // cout << '(' << graph[i][j].first << ',' << graph[i][j].second << ") ";
-
-        }
-    }
-    second++ ; 
-}
-
-void findTime() {
-    while(connectCount != n) {
-        eachSecond() ;
+void eachRound() {
+    while (1){
         bfs() ; 
+        if (nub < n) {
+            continue;
+        }
+        break; 
     }
-
-    cout << second << endl ;
+    cout << round ;
+    
 }
-
 int main() {
     readInput() ; 
-    findTime() ;
+    eachRound()  ;
 }
