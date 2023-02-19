692211d848fc0406e219dac1b760658878a76f72
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-02-20 00:02:08 +0700

    [ PPTPPTPP ]

--- week6/1-milk.cpp
+++ week6/1-milk.cpp
@@ -1,66 +1,49 @@
 #include<iostream>
-#include<queue>
 #include<vector>
+using namespace std ;
 
-using namespace std;
-
-int n, q, i, j;
+int n, q, i, j, u, v ; 
+pair<int, int> Edge[200001] ; 
+int root[100001] ; 
 char command ; 
-int u, v ;
 
-vector<int> graph[100001] ; 
-bool visited[100001] ; 
+int parent(int x) {
+    while (root[x] != x) {
+        root[x] = root[root[x]] ;
+        x = root[x] ; 
+    }
+    return x; 
+}
 
-void reset() {
-    int i ; 
-    for (i=0;i<n;i++) {
-        visited[i] = false ;
+void unionFind(int a, int b) {
+    int c = parent(a) ;
+    int d = parent(b) ;
+    root[c] = root[d] ; 
+}
+
+void init() {
+    for (i=1;i<=n;i++) {
+        root[i] = i ;
     }
 }
 
-void bfs(int start,int end) {
-    int i;
-    reset() ; 
-    queue <int> q ;
-    visited[start] = true ;
-    q.push(start) ;
-
-    while(!q.empty()) {
-        int curr = q.front() ;
-        q.pop() ; 
-
-        for (i=0;i < graph[curr].size();i++) {
-            int next = graph[curr][i] ;
-            if (next == end) {
-                cout << "yes\n" ;
-                return ;
-            }
-            if (!visited[next]) {
-                visited[next] = true ;
-                q.push(next) ; 
-            }
-        }
-    }
-    cout << "no\n" ; 
-    return ;
-}
-
-
-void readInput() {
+int main() {
     cin >> n >> q ;
+    init() ; 
     for (i=0;i<q;i++) {
         cin >> command >> u >> v ;
-        if (command == 'q') {
-            bfs(u,v) ; 
+
+        if (command == 'c') {
+            unionFind(u,v) ;
         }
+
         else {
-            graph[u].push_back(v) ; 
-            graph[v].push_back(u) ;
+            if (parent(u) == parent(v)) {
+                cout << "yes\n" ; 
+            }
+            else {
+                cout << "no\n" ; 
+            }
         }
     }
 }
-
-int main () {
-    readInput()  ;
-}
-
