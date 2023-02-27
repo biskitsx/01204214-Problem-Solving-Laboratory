fcb079c46b75eefe07d81dcaebad8618cde4e212
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-02-27 14:01:53 +0700

    not done

---
+++ week5/4-minimalcoverrage.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std ;
+
+int n, m, l=1, r=1; 
+int i, j;
+vector <pair<int,int> > segment ;  
+
+int main() {
+    cin >> n ;
+
+    for (i=0;i<n;i++) {
+        cin >> m ;
+        segment.resize(m) ;
+        j=0; 
+        
+        while (l != 0 && r != 0) {
+            cin >> segment[j].first >> segment[j].second ;
+        }
+    }
+}
